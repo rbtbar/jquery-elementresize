f6f3c8301b642c0a889c6aa8099731a7771173fd
Robert Bar <robert@rbsoftware.pl>
2014-09-10 15:28:57 +0200

    Add jquery dep&keywords to the bower; Add destroy function;
    Add jquery dep&keywords to the bower; Add destroy function;

--- jquery.size-trigger.js
+++ jquery.size-trigger.js
@@ -9,53 +9,65 @@
  *
  */
 ; (function ($, window, document, undefined) {
-	"use strict";
+    "use strict";
 
     var pluginName  = "sizetrigger"
-	    , defaults    = { onResize: undefined };
-	
-	function Plugin(element, options) {
-		this.element = element;
-		this.$element = $(element);
-		this.settings = $.extend(true, {}, defaults, options);
-		this._defaults = defaults;
-		this._name = pluginName;
-		this.init();
-	}
+      , defaults    = { onResize: undefined };
 
-	$.extend(Plugin.prototype, {
-		init: function () {
+    function Plugin(element, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.settings = $.extend(true, {}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this._init();
+    }
 
-			var iframes		    = [$('<iframe src="about:blank" style="position:absolute; top:-50000px; left:0px; width:100%;"></iframe>'), $('<iframe src="about:blank" style="position:absolute; top:0; left:-50000px; height:100%;"></iframe>')]
-			  , frameContent  = "<!DOCTYPE html><html><head><title>jquery.sizetrigger</title></head><body><script>window.onresize = resize;function resize() { var plugin = parent.$(this.frameElement).data('plugin_sizetrigger'); plugin.fireResizeFunction(); }</script></body></html>";
+    $.extend(Plugin.prototype, {
+        _init: function () {
 
-			for (var i = 0; i < iframes.length; i++) {
-				var $iframe = iframes[i];				
-				this.$element.append($iframe);
-				$iframe.data("plugin_" + pluginName, this);
-				$iframe[0].contentWindow.emitcontent = frameContent;
-				$iframe[0].src = "javascript:window.emitcontent";
-			}
+            var iframes       = [$('<iframe class="js-sizetrigger" src="about:blank" style="position:absolute; top:-50000px; left:0px; width:100%;"></iframe>'), $('<iframe src="about:blank" style="position:absolute; top:0; left:-50000px; height:100%;"></iframe>')]
+              , frameContent  = "<!DOCTYPE html><html><head><title>jquery.sizetrigger</title></head><body><script>window.onresize = resize;function resize() { var plugin = parent.$(this.frameElement).data('plugin_sizetrigger'); plugin._fireResizeFunction(); }</script></body></html>";
 
-			this.fireResizeFunction();
-		},
+            for (var i = 0; i < iframes.length; i++) {
+                var $iframe = iframes[i];
+                this.$element.append($iframe);
+                $iframe.data("plugin_" + pluginName, this);
+                $iframe[0].contentWindow.emitcontent = frameContent;
+                $iframe[0].src = "javascript:window.emitcontent";
+            }
 
-		fireResizeFunction: function () {
-			var fn = this.settings["onResize"];
+            this._iframes = iframes;
+            this._fireResizeFunction();
+        },
 
-			if ($.isFunction(fn)) {
-				fn();
-			}
-		}
-	});
+        _fireResizeFunction: function () {
+            var fn = this.settings["onResize"];
 
-	$.fn[pluginName] = function (options) {
-		this.each(function () {
-			if (!$.data(this, "plugin_" + pluginName)) {
-				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
-			}
-		});
-		return this;
-	};
+            if ($.isFunction(fn)) {
+                fn();
+            }
+        },
+
+        destroy: function() {
+            if (this.$element.data("plugin_" + pluginName)) {
+                this.$element.removeData("plugin_" + pluginName);
+                for (var i = 0; i < this._iframes.length; i++) {
+                    var $iframe = this._iframes[i];
+                    $iframe.removeData("plugin_" + pluginName);
+                    $iframe.remove();
+                }
+            }
+        }
+});
+
+$.fn[pluginName] = function (options) {
+    this.each(function () {
+        if (!$.data(this, "plugin_" + pluginName)) {
+            $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+        }
+    });
+    return this;
+};
 
 })(jQuery, window, document);
